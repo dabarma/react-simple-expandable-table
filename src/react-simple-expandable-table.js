bc5baf0bc5318991e242ef5c109fa52ba76da24c
David Barreda Marín <dbarreda@iti.es>
2022-06-29 09:58:15 +0200

    code format

--- src/react-simple-expandable-table.js
+++ src/react-simple-expandable-table.js
@@ -3,14 +3,14 @@ import './react-simple-expandable-table.css';
 import toggleCollapse from './img/toggle-collapse.png';
 import toggleExpand from './img/toggle-expand.png';
 
-export default function ReactSimpleExpandableTable(props){
+export default function ReactSimpleExpandableTable(props) {
 
     const [expandedRows, setExpandedRows] = useState([]);
 
-    const renderContent = ()=>{
+    const renderContent = () => {
 
-        return props.data.rows.map((row,index) =>{
-            return renderRow(row,index)
+        return props.data.rows.map((row, index) => {
+            return renderRow(row, index)
         });
     }
 
@@ -18,7 +18,7 @@ export default function ReactSimpleExpandableTable(props){
         return (
             <tr>
                 <th className="th-icon"></th>
-                {props.data.columns.map((column,index) =>{
+                {props.data.columns.map((column, index) => {
                     return <th key={index}>{column.description}</th>
                 })}
             </tr>)
@@ -33,41 +33,40 @@ export default function ReactSimpleExpandableTable(props){
 
         tds.push(<tr key={index}>
             {hasChilds ?
-            <td>{expanded ? 
-                <img className="expandCollapseIcon" style={{paddingLeft:(15*deep) + 'px'}} alt="collapse" src={toggleCollapse} onClick={()=> collapse(index)} /> : 
-                <img className="expandCollapseIcon" style={{paddingLeft:(15*deep) + 'px'}} alt="expand" src={toggleExpand} onClick={()=> expand(index)} />}</td> : 
-            <td></td>
+                <td>{expanded ?
+                    <img className="expandCollapseIcon" style={{ paddingLeft: (15 * deep) + 'px' }} alt="collapse" src={toggleCollapse} onClick={() => collapse(index)} /> :
+                    <img className="expandCollapseIcon" style={{ paddingLeft: (15 * deep) + 'px' }} alt="expand" src={toggleExpand} onClick={() => expand(index)} />}</td> :
+                <td></td>
             }
-            {props.data.columns.map((column,index) =>{
-                return  <td key={index}>{column.render ? column.render(row[column.dataBinding]) : row[column.dataBinding]}</td>
+            {props.data.columns.map((column, index) => {
+                return <td key={index}>{column.render ? column.render(row[column.dataBinding]) : row[column.dataBinding]}</td>
             })}</tr>);
 
-        if(expanded && hasChilds)
-        {
+        if (expanded && hasChilds) {
             row.rows.forEach((subRow, index) => {
-                tds.push(renderRow(subRow,index.toString() + '-' + deep + '-' + index.toString(),deep+1));
+                tds.push(renderRow(subRow, index.toString() + '-' + deep + '-' + index.toString(), deep + 1));
             });
         }
-          
+
         return tds;
     }
 
-    const expand = (index) =>{
-		setExpandedRows([index, ...expandedRows]);
-	}
+    const expand = (index) => {
+        setExpandedRows([index, ...expandedRows]);
+    }
 
     const collapse = (index) => {
-        setExpandedRows(expandedRows.filter(x=> x!==index));
-	}
+        setExpandedRows(expandedRows.filter(x => x !== index));
+    }
 
-    return(
+    return (
         <table className="expandable-table">
             <thead>
                 {renderHeaders()}
-            </thead>		
+            </thead>
             <tbody>
-               {renderContent()}
+                {renderContent()}
             </tbody>
         </table>
-    ) 
+    )
 }
